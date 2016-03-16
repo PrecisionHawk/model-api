d761bfc36536dd87ffbec74b7f3f602b87299aa1
Matthew Mead <m.mead@precisionhawk.com>
2016-03-16 12:49:29 -0400

    Groundwork for model-api gem.

---
+++ lib/model-api.rb
@@ -0,0 +1,38 @@
+require 'model-api/base_controller.rb'
+require 'model-api/bypass_parse_middleware.rb'
+require 'model-api/hash_metadata.rb'
+require 'model-api/model.rb'
+require 'model-api/not_found_exception.rb'
+require 'model-api/open_api_extensions.rb'
+require 'model-api/renderer.rb'
+require 'model-api/simple_metadata.rb'
+require 'model-api/suppress_login_redirect_middleware.rb'
+require 'model-api/unauthorized_exception.rb'
+require 'model-api/utils.rb'
+
+module ModelApi
+  class << self
+    def configure(metadata = nil, &block)
+      return unless metadata.is_a?(Hash) || block_given?
+      global_metadata = @model_api_global_metadata || default_global_metadata
+      if metadata.is_a?(Hash)
+        global_metadata = OpenApi::Utils.merge_hash(global_metadata, metadata)
+      end
+      if block_given?
+        config = OpenStruct.new(global_metadata)
+        block.call(config)
+        global_metadata = OpenApi::Utils.merge_hash(global_metadata, config.to_h.symbolize_keys)
+      end
+      @model_api_global_metadata = global_metadata
+    end
+    
+    def global_metadata
+      @model_api_global_metadata || default_global_metadata
+    end
+    
+    def default_global_metadata
+      {
+      }
+    end
+  end
+end
---
+++ model-api.gemspec
@@ -0,0 +1,23 @@
+lib = File.expand_path("../lib", __FILE__)
+$:.unshift lib unless $:.include?(lib)
+
+Gem::Specification.new do |s|
+  s.name = 'model-api'
+  s.version = '0.8.0'
+  s.summary = 'Create easy REST API\'s using metadata inside your ActiveRecord models'
+  s.description = 'Ruby gem allowing Ruby on Rails developers to create REST API’s using ' \
+      'metadata defined inside their ActiveRecord models.'
+  s.licenses = ['Apache 2']
+  s.authors = ['Matthew Mead']
+  s.email = 'm.mead@precisionhawk.com'
+
+  s.files = Dir.glob("{lib,spec,config}/**/*")
+  s.files += %w(model-api.gemspec README.md)
+
+  s.require_path = "lib"
+
+  s.add_dependency "rails", ">= 4.0"
+
+  s.add_development_dependency "rspec-rails"
+  s.add_development_dependency "factory_girl"
+end
